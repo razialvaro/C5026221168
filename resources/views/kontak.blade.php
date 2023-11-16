9f866137deefb7b18024ff70651e09f592ecf6c8
razialvaro <82017050+razialvaro@users.noreply.github.com>
2023-11-16 15:21:36 +0700

    Pertemuan Tanggal 16 Nov

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,56 @@
+@extends('master')
+
+@section('judulhalaman', 'Kontak')
+
+@section('judul')
+<div class="jumbotron">
+    Ini Halaman Kontak
+</div>
+
+@endsection
+
+@section('konten')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Bordered Table</h2>
+  <p>The .table-bordered class adds borders on all sides of the table and the cells:</p>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      @for($i=1;$i<6;$i++)
+      <tr>
+        <td>No</td>
+        <td>:</td>
+        <td>{{ $i }}</td>
+      </tr>
+      @endfor
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
+@endsection
